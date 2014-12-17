383db5f3378e08a1f2cc426aef7f33ca3cf8959c
E.T.Cook <e@etc.io>
2014-12-17 13:15:06 -0600

    formatting plus bugfix for non standard templates

--- public/includes/template-loader.php
+++ public/includes/template-loader.php
@@ -23,11 +23,11 @@ class PT_Template_Loader {
          */
         public static function get_instance() {
 
-                if( null == self::$instance ) {
-                        self::$instance = new PT_Template_Loader();
-                }
+            if( null == self::$instance ) {
+                    self::$instance = new PT_Template_Loader();
+            }
 
-                return self::$instance;
+            return self::$instance;
 
         } 
 
@@ -36,35 +36,35 @@ class PT_Template_Loader {
          */
         private function __construct() {
 
-                $this->templates = array();
+            $this->templates = array();
 
 
-                // Add a filter to the attributes metabox to inject template into the cache.
-                add_filter(
-                    'page_attributes_dropdown_pages_args',
-                     array( $this, 'register_project_templates' ) 
-                );
+            // Add a filter to the attributes metabox to inject template into the cache.
+            add_filter(
+                'page_attributes_dropdown_pages_args',
+                 array( $this, 'register_project_templates' )
+            );
 
 
-                // Add a filter to the save post to inject out template into the page cache
-                add_filter(
-                    'wp_insert_post_data', 
-                    array( $this, 'register_project_templates' ) 
-                );
+            // Add a filter to the save post to inject out template into the page cache
+            add_filter(
+                'wp_insert_post_data', 
+                array( $this, 'register_project_templates' ) 
+            );
 
 
-                // Add a filter to the template include to determine if the page has our 
-                // template assigned and return it's path
-                add_filter(
-                    'template_include', 
-                    array( $this, 'view_project_template') 
-                );
+            // Add a filter to the template include to determine if the page has our 
+            // template assigned and return it's path
+            add_filter(
+                'template_include', 
+                array( $this, 'view_project_template') 
+            );
 
 
-                // Add your templates to this array.
-                $this->templates = array(
-                        'template/pt-wp-discourse-sso.php'     => 'PT WP Discourse SSO',
-                );
+            // Add your templates to this array.
+            $this->templates = array(
+                    'template/pt-wp-discourse-sso.php'     => 'PT WP Discourse SSO',
+            );
                 
         } 
 
@@ -77,28 +77,28 @@ class PT_Template_Loader {
 
         public function register_project_templates( $atts ) {
 
-                // Create the key used for the themes cache
-                $cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+            // Create the key used for the themes cache
+            $cache_key = 'page_templates-' . md5( get_theme_root(get_stylesheet()) . '/' . get_stylesheet() );
 
-                // Retrieve the cache list. 
-                // If it doesn't exist, or it's empty prepare an array
-                $templates = wp_get_theme()->get_page_templates();
-                if ( empty( $templates ) ) {
-                        $templates = array();
-                } 
+            // Retrieve the cache list. 
+            // If it doesn't exist, or it's empty prepare an array
+            $templates = wp_get_theme()->get_page_templates();
+            if ( empty( $templates ) ) {
+                    $templates = array();
+            } 
 
-                // New cache, therefore remove the old one
-                wp_cache_delete( $cache_key , 'themes');
+            // New cache, therefore remove the old one
+            wp_cache_delete( $cache_key , 'themes');
 
-                // Now add our template to the list of templates by merging our templates
-                // with the existing templates array from the cache.
-                $templates = array_merge( $templates, $this->templates );
+            // Now add our template to the list of templates by merging our templates
+            // with the existing templates array from the cache.
+            $templates = array_merge( $templates, $this->templates );
 
-                // Add the modified cache to allow WordPress to pick it up for listing
-                // available templates
-                wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+            // Add the modified cache to allow WordPress to pick it up for listing
+            // available templates
+            wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 
-                return $atts;
+            return $atts;
 
         } 
 
@@ -107,27 +107,27 @@ class PT_Template_Loader {
          */
         public function view_project_template( $template ) {
 
-                global $post;
+            global $post;
 
-                if (!isset($this->templates[get_post_meta( 
-                    $post->ID, '_wp_page_template', true 
-                )] ) ) {
-                    
-                        return $template;
-                        
-                } 
-
-                $file = plugin_dir_path(__FILE__). get_post_meta( 
-                    $post->ID, '_wp_page_template', true 
-                );
+            if (!isset($this->templates[get_post_meta( 
+                $post->ID, '_wp_page_template', true 
+            )] ) ) {
                 
-                // Just to be safe, we check if the file exist first
-                if( file_exists( $file ) ) {
-                        return $file;
-                } 
-                else { echo $file; }
+                    return $template;
+                    
+            } 
 
-                return $template;
+            $file = plugin_dir_path(__FILE__). get_post_meta( 
+                $post->ID, '_wp_page_template', true 
+            );
+            
+            // Just to be safe, we check if the file exist first
+            if( file_exists( $file ) ) {
+                    return $file;
+            } 
+            else { echo $file; }
+
+            return $template;
 
         } 
 
